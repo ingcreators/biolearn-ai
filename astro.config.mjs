f3df0f9abcd2e8f52a9af4bdf06aeff446dc7c5d
Naoki Ichimura <ichimura@gmail.com>
2026-05-24 08:10:14 +0900

    Organize sidebar by roadmap chapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -80,81 +80,109 @@ export default defineConfig({
         {
           label: '学習ロードマップ',
           items: [
-            { label: '00. はじめに', link: '/getting-started/' },
+            { label: 'はじめに', link: '/getting-started/' },
             { label: '学習ロードマップ', link: '/roadmap/' },
             {
-              label: '01. 生命科学とは何か',
-              link: '/getting-started/what-is-life-science/',
-            },
-            { label: '02. 細胞とは何か', link: '/basics/what-is-a-cell/' },
-            {
-              label: '03. DNAとは何か',
-              link: '/molecular-biology/what-is-dna/',
-            },
-            {
-              label: '04. RNAとは何か',
-              link: '/molecular-biology/what-is-rna/',
+              label: 'はじめに：生命科学の地図を作る',
+              items: [
+                {
+                  label: '01. 生命科学とは何か',
+                  link: '/getting-started/what-is-life-science/',
+                },
+                { label: '02. 細胞とは何か', link: '/basics/what-is-a-cell/' },
+              ],
             },
             {
-              label: '05. タンパク質とは何か',
-              link: '/molecular-biology/what-is-protein/',
+              label: '第1章 分子生物学：DNAからタンパク質へ',
+              items: [
+                {
+                  label: '03. DNAとは何か',
+                  link: '/molecular-biology/what-is-dna/',
+                },
+                {
+                  label: '04. RNAとは何か',
+                  link: '/molecular-biology/what-is-rna/',
+                },
+                {
+                  label: '05. タンパク質とは何か',
+                  link: '/molecular-biology/what-is-protein/',
+                },
+                {
+                  label: '06. 遺伝子とは何か',
+                  link: '/molecular-biology/what-is-gene/',
+                },
+                {
+                  label: '07. セントラルドグマとは何か',
+                  link: '/molecular-biology/what-is-central-dogma/',
+                },
+                {
+                  label: '08. 転写とは何か',
+                  link: '/molecular-biology/what-is-transcription/',
+                },
+                {
+                  label: '09. 翻訳とは何か',
+                  link: '/molecular-biology/what-is-translation/',
+                },
+                {
+                  label: '10. 遺伝子発現とは何か',
+                  link: '/molecular-biology/what-is-gene-expression/',
+                },
+              ],
             },
             {
-              label: '06. 遺伝子とは何か',
-              link: '/molecular-biology/what-is-gene/',
+              label: '第2章 細胞生物学：分子が働く場所を理解する',
+              items: [
+                {
+                  label: '11. 細胞膜とは何か',
+                  link: '/cell-biology/what-is-cell-membrane/',
+                },
+                {
+                  label: '12. 細胞小器官とは何か',
+                  link: '/cell-biology/what-are-organelles/',
+                },
+                {
+                  label: '13. 細胞シグナル伝達とは何か',
+                  link: '/cell-biology/what-is-cell-signaling/',
+                },
+              ],
             },
             {
-              label: '07. セントラルドグマとは何か',
-              link: '/molecular-biology/what-is-central-dogma/',
+              label: '第3章 ゲノミクス：遺伝情報を全体で捉える',
+              items: [
+                {
+                  label: '14. ゲノムとは何か',
+                  link: '/genomics/what-is-genome/',
+                },
+                {
+                  label: '15. 染色体とは何か',
+                  link: '/genomics/what-are-chromosomes/',
+                },
+                {
+                  label: '16. 遺伝的変異とは何か',
+                  link: '/genomics/what-are-genetic-variants/',
+                },
+              ],
             },
             {
-              label: '08. 転写とは何か',
-              link: '/molecular-biology/what-is-transcription/',
-            },
-            {
-              label: '09. 翻訳とは何か',
-              link: '/molecular-biology/what-is-translation/',
-            },
-            {
-              label: '10. 遺伝子発現とは何か',
-              link: '/molecular-biology/what-is-gene-expression/',
-            },
-            {
-              label: '11. 細胞膜とは何か',
-              link: '/cell-biology/what-is-cell-membrane/',
-            },
-            {
-              label: '12. 細胞小器官とは何か',
-              link: '/cell-biology/what-are-organelles/',
-            },
-            {
-              label: '13. 細胞シグナル伝達とは何か',
-              link: '/cell-biology/what-is-cell-signaling/',
-            },
-            { label: '14. ゲノムとは何か', link: '/genomics/what-is-genome/' },
-            {
-              label: '15. 染色体とは何か',
-              link: '/genomics/what-are-chromosomes/',
-            },
-            {
-              label: '16. 遺伝的変異とは何か',
-              link: '/genomics/what-are-genetic-variants/',
-            },
-            {
-              label: '17. PCRとは何か',
-              link: '/molecular-biology/what-is-pcr/',
-            },
-            {
-              label: '18. RNA-seqとは何か',
-              link: '/bioinformatics/what-is-rna-seq/',
-            },
-            {
-              label: '19. シーケンスデータとは何か',
-              link: '/bioinformatics/what-is-sequence-data/',
-            },
-            {
-              label: '20. 差次的発現解析とは何か',
-              link: '/bioinformatics/what-is-differential-expression-analysis/',
+              label: '第4章 実験とバイオインフォマティクス：測定からデータへ',
+              items: [
+                {
+                  label: '17. PCRとは何か',
+                  link: '/molecular-biology/what-is-pcr/',
+                },
+                {
+                  label: '18. RNA-seqとは何か',
+                  link: '/bioinformatics/what-is-rna-seq/',
+                },
+                {
+                  label: '19. シーケンスデータとは何か',
+                  link: '/bioinformatics/what-is-sequence-data/',
+                },
+                {
+                  label: '20. 差次的発現解析とは何か',
+                  link: '/bioinformatics/what-is-differential-expression-analysis/',
+                },
+              ],
             },
           ],
         },
